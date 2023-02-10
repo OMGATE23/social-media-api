5e9b422020538785dfb3cbffa36a7d207d8c9469
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-02-10 21:27:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const format = require('date-format')
+
+const PORT = 3000 || process.env.PORT
+
+
+app.get("/" , (req, res) => {
+    res.status(200).send("<h1>I am Amongus</h1>")
+})
+
+app.get("/api/v1/instagram" , (req, res) => {
+    const instaSocial = {
+        username : "omgate",
+        followers: 200,
+        follows: 1,
+        date : format.asString("dd-mm-yyyy | hh:mm:ss" , new Date())
+    }
+
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/facebook" , (req, res) => {
+    const facebookSocial = {
+        username : "omgatefacebook",
+        followers: 452,
+        follows: 123,
+        date : format.asString("dd-mm-yyyy | hh:mm:ss" , new Date())
+    }
+
+    res.status(200).json(facebookSocial)
+})
+
+app.get("/api/v1/linkedIn" , (req, res) => {
+    const linkedInSocial = {
+        username : "omgateLinkedIN",
+        followers: 12333,
+        follows: 43,
+        date : format.asString("dd-mm-yyyy | hh:mm:ss" , new Date())
+    }
+
+    res.status(200).json( linkedInSocial)
+})
+
+app.get("/api/v1/:token" , (req, res) => {
+    console.log(req.params.token)
+    res.status(200).json({
+        param : req.params.token
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is runnning at ${PORT}`)
+})
